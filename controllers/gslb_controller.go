a7aa94cf736866f068d2a471db1545075a6afbeb
Mohammad Yosefpor <mohammad.yosefpor@snapp.cab>
2021-06-09 04:04:25 +0430

    simple controller

--- controllers/gslb_controller.go
+++ controllers/gslb_controller.go
@@ -18,11 +18,13 @@ package controllers
 
 import (
 	"context"
+	"reflect"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
 
 	gslbv1alpha1 "github.com/snapp-cab/gslb-controller/api/v1alpha1"
 )
@@ -47,11 +49,61 @@ type GslbReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *GslbReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := ctrllog.FromContext(ctx)
 
-	// your logic here
+	// Lookup the route instance for this reconcile request
+	gslb := &gslbv1alpha1.Gslb{}
+	err := r.Get(ctx, req.NamespacedName, gslb)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			log.Info("Resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		log.Error(err, "Failed to get Routes")
+		return ctrl.Result{}, err
+	}
 
+	// Check if the service already exists, if not create a new one
+	currentGslb, err := GetGslb(ctx, gslb)
+	if err != nil && errors.IsNotFound(err) {
+		log.Info("Creating a new gslb service", "Gslb.Namespace", gslb.Namespace, "Gslb.Name", gslb.Name)
+		err = CreateGslb(ctx, gslb)
+		if err != nil {
+			log.Error(err, "Failed to create new gslb service", "Gslb.Namespace", gslb.Namespace, "Gslb.Name", gslb.Name)
+			return ctrl.Result{}, err
+		}
+		// Deployment created successfully - return and requeue
+		return ctrl.Result{Requeue: true}, nil
+	} else if err != nil {
+		log.Error(err, "Failed to get gslb service")
+		return ctrl.Result{}, err
+	}
+
+	// Ensure the gslb service status with the desired state
+	if !reflect.DeepEqual(currentGslb, gslb) {
+		err := UpdateGslb(ctx, gslb)
+		if err != nil {
+			log.Error(err, "Failed to update the gslb service", "Gslb.Namespace", gslb.Namespace, "Gslb.Name", gslb.Name)
+			return ctrl.Result{}, err
+		}
+	}
 	return ctrl.Result{}, nil
+}
+
+func GetGslb(ctx context.Context, gslb *gslbv1alpha1.Gslb) (*gslbv1alpha1.Gslb, error) {
+	return gslb, nil
+}
+
+func CreateGslb(ctx context.Context, gslb *gslbv1alpha1.Gslb) error {
+	return nil
+}
+
+func UpdateGslb(ctx context.Context, gslb *gslbv1alpha1.Gslb) error {
+	return nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
