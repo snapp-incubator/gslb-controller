4f693a45cb03f0244bd11c533a9dc440eca2b631
Mohammad Yosefpor <mohammad.yosefpor@snapp.cab>
2021-06-09 18:39:52 +0430

    add healthChecks

--- controllers/consul.go
+++ controllers/consul.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"context"
 	"fmt"
+	"time"
 
 	consulapi "github.com/hashicorp/consul/api"
 	gslbv1alpha1 "github.com/snapp-cab/gslb-controller/api/v1alpha1"
@@ -49,22 +50,46 @@ func UpdateGslb(ctx context.Context, gslb *gslbv1alpha1.Gslb) error {
 }
 
 func CreateOrUpdateGslb(ctx context.Context, gslb *gslbv1alpha1.Gslb) error {
-	reg := &consulapi.CatalogRegistration{
-		Node:    gslb.Namespace + "-" + gslb.Name,
-		Address: gslb.Spec.Host,
-		NodeMeta: map[string]string{
-			"external-node":  "true",
-			"external-probe": "false",
-		},
-		Service: &consulapi.AgentService{
-			ID:      gslb.Name,
-			Service: gslb.Name,
-		},
-		// Checks: consulapi.HealthChecks{},
+	// TODO check if serviceName is not used, else raise error
+	for _, b := range gslb.Spec.Backends {
+		var probeHost string
+		if b.Probe.HTTPGet.Host == "" {
+			probeHost = b.Host
+		} else {
+			probeHost = b.Probe.HTTPGet.Host
+		}
+		header := make(map[string][]string)
+		for _, h := range b.Probe.HTTPGet.HTTPHeaders {
+			header[h.Name] = []string{h.Value}
+		}
+		reg := &consulapi.CatalogRegistration{
+			Node:    gslb.Namespace + "-" + gslb.Name + "-" + b.Name,
+			Address: b.Host,
+			NodeMeta: map[string]string{
+				"external-node":  "true",
+				"external-probe": "false",
+			},
+			Service: &consulapi.AgentService{
+				ID:      gslb.Spec.ServiceName,
+				Service: gslb.Spec.ServiceName,
+			},
+			Checks: consulapi.HealthChecks{
+				&consulapi.HealthCheck{
+					Name: "http-check",
+					Definition: consulapi.HealthCheckDefinition{
+						HTTP:             probeHost,
+						IntervalDuration: time.Duration(b.Probe.PeriodSeconds),
+						TimeoutDuration:  time.Duration(b.Probe.TimeoutSeconds),
+						Header:           header,
+					},
+				},
+			},
+		}
+		_, err := consul.cat.Register(reg, &consulapi.WriteOptions{})
+		if err != nil {
+			return fmt.Errorf("failed to create consul service: %w", err)
+		}
 	}
-	_, err := consul.cat.Register(reg, &consulapi.WriteOptions{})
-	if err != nil {
-		return fmt.Errorf("failed to create consul service: %w", err)
-	}
+
 	return nil
 }
